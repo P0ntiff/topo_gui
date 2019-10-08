ec90876c6160c4ea4cdc09b6e44e0146f1922198
COMS4200 <gattasb@gmail.com>
2019-10-08 17:57:00 +1000

    Wrapped code in a class, added command line run option, added README.md

--- buildGraph.py
+++ buildGraph.py
@@ -1,186 +1,186 @@
 import json
 import requests
 import time
-
-
-FLOODLIGHT_PORT = 8080
-FLOODlIGHT_BASE_URL = 'http://localhost:' + str(FLOODLIGHT_PORT) + '/wm/'
-
-
-CONTROLLER_ID = 'controller'
+import sys
 
 
 
-def querySwitches():
-    switchListRequest = requests.get(FLOODlIGHT_BASE_URL + 'core/controller/switches/json')
-    if switchListRequest.status_code == 200:
-        switches = switchListRequest.json()
-        switchList = [switch['dpid'] for switch in switches]
-        buildSwitchesJSON(switchList)
-        return 1
-    else:
-        print('Error making GET request')
-        print(switchListRequest.text)
-        return -1
+class GraphBuilder:
+    def __init__(self):
+        self.floodlightPort = 8080
+        self.floodlightBaseURL = 'http://localhost:' + str(self.floodlightPort) + '/wm/'
 
-def buildSwitchesJSON(switchList):
-    outputGraph = {
-        'nodes': [],
-        'edges': []
-    }
-    controllerNode = {'data' : {'id' : CONTROLLER_ID, 'name': 'Controller'}, 'classes': "controller"}
-    outputGraph['nodes'].append(controllerNode)
+        self.controllerID = 'controller'
+        self.switchBaseID = 'switch_'
 
-    # build in switches with connections to controller
-    for switch in switchList:
-        # assuming up to 10 dpid's for now
-        switchID = 'switch_' + switch[-2:]
-        switchName = 'Switch ' + switch[-2:]
-        # append node
-        node = {'data' : {'id' : switchID, 'name' : switchName}, 'classes': 'switch'}
-        outputGraph['nodes'].append(node)
+    def querySwitches(self):
+        switchListRequest = requests.get(self.floodlightBaseURL + 'core/controller/switches/json')
+        if switchListRequest.status_code == 200:
+            switches = switchListRequest.json()
+            switchList = [switch['dpid'] for switch in switches]
+            self.buildSwitchesJSON(switchList)
+            return 1
+        else:
+            print('Error making GET request')
+            print(switchListRequest.text)
+            return -1
 
-        # append edge
-        edge = {'data': { 'id': CONTROLLER_ID + '_to_' + switchID, 'weight': 1, 'source': CONTROLLER_ID, 'target': switchID}}
-        outputGraph['edges'].append(edge)
+    def buildSwitchesJSON(self, switchList):
+        outputGraph = {
+            'nodes': [],
+            'edges': []
+        }
+        controllerNode = {'data' : {'id' : self.controllerID, 'name': 'Controller'}, 'classes': "controller"}
+        outputGraph['nodes'].append(controllerNode)
 
-    # add in some default positions for the layout code to work
-    for node in outputGraph['nodes']:
-        node['position'] = {'x': 1, 'y': 1}
-
-    # publish to file
-    with open('networkGraph.json', 'w') as f:
-        json.dump(outputGraph, f)
-
-
-def queryStats():
-    statsRequest = requests.get(FLOODlIGHT_BASE_URL + 'core/switch/all/aggregate/json')
-
-    if statsRequest.status_code == 200:
-        statMap = statsRequest.json()
-        buildStatsJSON(statMap)
-    else:
-        print('Error making GET request')
-        print(statsRequest.text)
-
-
-def buildStatsJSON(statMap):
-    outputObject = {}
-
-    for switch in statMap:
-        packetCount = statMap[switch][0]['packetCount']
-        byteCount = statMap[switch][0]['byteCount']
-        flowCount = statMap[switch][0]['flowCount']
-        switchID = 'switch_' + switch[-2:]
-
-        # ouptut node with stats
-        node = {'data' : 
-                    {'id' : switchID, 
-                    'packetCount' : packetCount, 
-                    'byteCount' : byteCount, 
-                    'flowCount' : flowCount}}
-        outputObject[switchID] = node
-
-    # publish to file
-    with open('networkStats.json', 'w') as f:
-        json.dump(outputObject, f)
-
-
-def queryHostsAndLinks():
-    devicesRequest = requests.get(FLOODlIGHT_BASE_URL + 'device/')
-    linksRequest = requests.get(FLOODlIGHT_BASE_URL + 'topology/links/json')
-
-    if (devicesRequest.status_code == 200) and (linksRequest.status_code == 200):
-        deviceData = devicesRequest.json()
-        linkData = linksRequest.json()
-        buildHostsAndLinksJSON(deviceData, linkData)
-    else:
-        print('Error making GET request')
-        print(devicesRequest.text)
-        print(linksRequest.text)
-
-def buildHostsAndLinksJSON(deviceData, linkData):
-    outputGraph = {
-        'nodes': [],
-        'edges': []
-    }
-    # hosts
-    for device in deviceData:
-        # assume that a recorded device with an IPv4 is a host (weak but need to get around fake hosts in TopoGuard+)
-        if len(device['ipv4']) > 0:
-            ipAddr = device['ipv4'][0]
-            hostID = 'host_' + ipAddr
-            macAddr = device['mac']
-            if 'attachmentPoint' not in device:
-                print('Attachment point not recorded for device ' + ipAddr)
-                return
-            attachedSwitch = device['attachmentPoint'][0]['switchDPID'][-2:]
-            attachedSwitchID = 'switch_' + attachedSwitch
-            attachedSwitchPort = device['attachmentPoint'][0]['port']
-
-            # output host as a node with these fields
+        # build in switches with connections to controller
+        for switch in switchList:
+            # assuming up to 10 dpid's for now
+            switchID = self.switchBaseID + switch[-2:]
+            switchName = 'Switch ' + switch[-2:]
             # append node
-            node = {'data' : 
-                        {'id' : hostID, 
-                        'name' : ipAddr}, 
-                    'classes': 'host'}
+            node = {'data' : {'id' : switchID, 'name' : switchName}, 'classes': 'switch'}
             outputGraph['nodes'].append(node)
 
-            # append edge from host to attached switch 
-            edge = {'data': 
-                        {'id': attachedSwitchID + '_to_' + hostID, 
-                         'weight': 1,
-                         'source': attachedSwitchID, 
-                         'target': hostID,
-                         'sourceSwitchPort': attachedSwitchPort},
-                    'classes': 'host-switch-link'}
+            # append edge
+            edge = {'data': { 'id': self.controllerID + '_to_' + switchID, 'weight': 1, 'source': self.controllerID, 'target': switchID}}
             outputGraph['edges'].append(edge)
 
-    # add in some default positions for the layout code to work
-    for node in outputGraph['nodes']:
-        node['position'] = {'x': 1, 'y': 1}
+        # add in some default positions for the layout code to work
+        for node in outputGraph['nodes']:
+            node['position'] = {'x': 1, 'y': 1}
 
-    # inter-switch links
-    for link in linkData:
-        sourceSwitchID = 'switch_' + link['src-switch'][-2:]
-        destSwitchID = 'switch_' + link['dst-switch'][-2:]
-        sourceSwitchPort = link['src-port']
-        destSwitchPort = link['dst-port']
-
-        # append edge from switch to other switch 
-        edge = {'data': 
-                    {'id': sourceSwitchID + '_to_' + destSwitchID, 
-                        'weight': 1,
-                        'source': sourceSwitchID, 
-                        'target': destSwitchID,
-                        'sourceSwitchPort': sourceSwitchPort,
-                        'destSwitchPort': destSwitchPort},
-                'classes': 'switch-switch-link'}
-        outputGraph['edges'].append(edge)
-
-    # publish to file
-    with open('networkHostsAndLinks.json', 'w') as f:
-        json.dump(outputGraph, f)
+        # publish to file
+        with open('networkGraph.json', 'w') as f:
+            json.dump(outputGraph, f)
 
 
+    def queryStats(self):
+        statsRequest = requests.get(self.floodlightBaseURL + 'core/switch/all/aggregate/json')
 
-res = querySwitches()
-if res == -1:
-    sys.exit()
-
-
-queryStats()
-
-
-queryHostsAndLinks()
-
-# counter = 0
-# while counter < 50:
-#     print('Querying REST API. Count #' + str(counter))
-#     queryStats()
-#     time.sleep(1)
-#     counter += 1
+        if statsRequest.status_code == 200:
+            statMap = statsRequest.json()
+            self.buildStatsJSON(statMap)
+        else:
+            print('Error making GET request')
+            print(statsRequest.text)
 
 
+    def buildStatsJSON(self, statMap):
+        outputObject = {}
 
-# time.sleep(2)
+        for switch in statMap:
+            packetCount = statMap[switch][0]['packetCount']
+            byteCount = statMap[switch][0]['byteCount']
+            flowCount = statMap[switch][0]['flowCount']
+            switchID = self.switchBaseID + switch[-2:]
+
+            # ouptut node with stats
+            node = {'data' : 
+                        {'id' : switchID, 
+                        'packetCount' : packetCount, 
+                        'byteCount' : byteCount, 
+                        'flowCount' : flowCount}}
+            outputObject[switchID] = node
+
+        # publish to file
+        with open('networkStats.json', 'w') as f:
+            json.dump(outputObject, f)
+
+
+    def queryHostsAndLinks(self):
+        devicesRequest = requests.get(self.floodlightBaseURL + 'device/')
+        linksRequest = requests.get(self.floodlightBaseURL + 'topology/links/json')
+
+        if (devicesRequest.status_code == 200) and (linksRequest.status_code == 200):
+            deviceData = devicesRequest.json()
+            linkData = linksRequest.json()
+            self.buildHostsAndLinksJSON(deviceData, linkData)
+        else:
+            print('Error making GET request')
+            print(devicesRequest.text)
+            print(linksRequest.text)
+
+    def buildHostsAndLinksJSON(self, deviceData, linkData):
+        outputGraph = {
+            'nodes': [],
+            'edges': []
+        }
+
+        # hosts
+        for device in deviceData:
+            # assume that a recorded device with an IPv4 is a host (weak but need to get around fake hosts in TopoGuard+)
+            if len(device['ipv4']) > 0:
+                ipAddr = device['ipv4'][0]
+                hostID = 'host_' + ipAddr
+                macAddr = device['mac']
+                if 'attachmentPoint' not in device:
+                    print('Attachment point not recorded for device ' + ipAddr)
+                    return
+                attachedSwitch = device['attachmentPoint'][0]['switchDPID'][-2:]
+                attachedSwitchID = self.switchBaseID + attachedSwitch
+                attachedSwitchPort = device['attachmentPoint'][0]['port']
+
+                # output host as a node with these fields
+                # append node
+                node = {'data' : 
+                            {'id' : hostID, 
+                            'name' : ipAddr}, 
+                        'classes': 'host'}
+                outputGraph['nodes'].append(node)
+
+                # append edge from host to attached switch 
+                edge = {'data': 
+                            {'id': attachedSwitchID + '_to_' + hostID, 
+                            'weight': 1,
+                            'source': attachedSwitchID, 
+                            'target': hostID,
+                            'sourceSwitchPort': attachedSwitchPort},
+                        'classes': 'host-switch-link'}
+                outputGraph['edges'].append(edge)
+
+        # add in some default positions for the layout code to work
+        for node in outputGraph['nodes']:
+            node['position'] = {'x': 1, 'y': 1}
+
+        # inter-switch links
+        for link in linkData:
+            sourceSwitchID = self.switchBaseID + link['src-switch'][-2:]
+            destSwitchID = self.switchBaseID + link['dst-switch'][-2:]
+            sourceSwitchPort = link['src-port']
+            destSwitchPort = link['dst-port']
+
+            # append edge from switch to other switch 
+            edge = {'data': 
+                        {'id': sourceSwitchID + '_to_' + destSwitchID, 
+                            'weight': 1,
+                            'source': sourceSwitchID, 
+                            'target': destSwitchID,
+                            'sourceSwitchPort': sourceSwitchPort,
+                            'destSwitchPort': destSwitchPort},
+                    'classes': 'switch-switch-link'}
+            outputGraph['edges'].append(edge)
+
+        # publish to file
+        with open('networkHostsAndLinks.json', 'w') as f:
+            json.dump(outputGraph, f)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: buildGraph.py <poll-time-in-seconds>')
+        sys.exit()
+    pollTime = int(sys.argv[1])
+    gb = GraphBuilder()
+    res = gb.querySwitches()
+    if res == -1:
+        print('Could not connect to REST API to get switches')
+        sys.exit()
+    gb.queryHostsAndLinks()
+    counter = 0
+    while True:
+        print('Querying REST API. Count #' + str(counter))
+        gb.queryStats()
+        time.sleep(pollTime)
+        counter += 1
+
